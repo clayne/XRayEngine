e5c649525560a60aff0b0b65cd1532ff0ed4df94
Romann <kriworuck.production@yandex.ru>
2023-08-04 04:49:19 +0300

    * [cibuild]

--- utils/misc/xr_pack_build.cmd
+++ utils/misc/xr_pack_build.cmd
@@ -25,5 +25,5 @@ set EDITION_NAME=%EDITION_NAME: =.%
 @echo on
 
 rem Make archives
-7z a "HybridXRay.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.dll Bin\%PLATFORM%\%CONFIGURATION%\*.exe gamedata -xr!.* rawdata -xr!.* -i!utils\oalinst.exe -i!tool_compile_xrAI.cmd -i!tool_compile_xrAI_draft.cmd -i!tool_compile_xrDO.cmd -i!tool_compile_xrLC.cmd -i!tool_create_spawn.cmd -i!tool_verify_ai_map.cmd -i!fs.ltx -i!fs_cs.ltx -i!fs_soc.ltx -i!fsfactory.ltx -i!fsgame.ltx -i!fsgame_cs.ltx -i!fsgame_soc.ltx -i!License.txt
+7z a "HybridXRay.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.dll Bin\%PLATFORM%\%CONFIGURATION%\*.exe Bin\compilers_sky_x64 -xr!.* gamedata -xr!.* rawdata -xr!.* -i!utils\oalinst.exe -i!utils\tool_compile_xrAI.cmd -i!utils\tool_compile_xrAI_draft.cmd -i!utils\tool_compile_xrDO.cmd -i!utils\tool_compile_xrLC.cmd -i!utils\tool_create_spawn.cmd -i!utils\tool_verify_ai_map.cmd -i!_ActorEditor.cmd -i!_LevelEditor.cmd -i!_ParticleEditor.cmd -i!_ShaderEditor.cmd -i!Compiler_AI_map.cmd -i!Compiler_Batch.cmd -i!Compiler_Details.cmd -i!Compiler_Geometry.cmd -i!Compiler_Make_spawn.cmd -i!Compiler_Menu.cmd -i!Compiler_Readme.txt -i!fs.ltx -i!fs_cs.ltx -i!fs_soc.ltx -i!fsfactory.ltx -i!fsgame.ltx -i!fsgame_cs.ltx -i!fsgame_soc.ltx -i!License.txt
 7z a "Symbols.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.pdb -i!License.txt -xr!.*
